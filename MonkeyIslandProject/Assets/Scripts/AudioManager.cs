b6367aed828200c69ec7ceaa9cd47f4c12371e62
yasmin-alshurafa <70956078+yasmin-alshurafa@users.noreply.github.com>
2022-11-13 20:32:31 +0400

    push

---
+++ MonkeyIslandProject/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager audioManagerInstance;
+    public Sound[] sounds;
+    private void Awake()
+    {
+        if(audioManagerInstance==null)
+        {
+            audioManagerInstance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+        foreach(Sound s in sounds)
+        {
+            s.source=gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+    private void Start()
+    {
+        Play("BGM");
+    }
+    public void Play(string name)
+    {
+        Sound s=Array.Find(sounds, sound => sound.name == name);
+        if(s==null)
+        {
+            return;
+        }
+        s.source.Play();
+    }
+    public void Pause(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            return;
+        }
+        s.source.Stop();
+    }
+    public void ReduceVolume(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            return;
+        }
+        s.source.volume = 0f;
+    }
+    public void RaiseVolume(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            return;
+        }
+        s.source.volume = 0.78f;
+    }
+}
